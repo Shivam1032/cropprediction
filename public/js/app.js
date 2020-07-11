7ff2be21e202a3476bcab99f1dffa48d02117dbf
animeshaks <animeshshukla98279@gmail.com>
2020-07-11 22:20:26 +0530

    nothing

--- public/js/app.js
+++ public/js/app.js
@@ -49646,8 +49646,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\SIH\WEBSITE\kbfms\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\SIH\WEBSITE\kbfms\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\SIH\KrishiBodh\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\SIH\KrishiBodh\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
