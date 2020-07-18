6128ff2b33e85c47b8a3e384171cc7d8e32275e0
ROCKYCODINGG <nicemohit260@gmail.com>
2020-07-18 22:55:16 +0530

    added request aids

--- resources/js/app.js
+++ resources/js/app.js
@@ -44,10 +44,12 @@ const options = {
 let routes =
 [
     {path: '/user',component:require('./components/user.vue').default},
-    {path: '/soiltest',component:require('./components/soiltesting.vue').default},
+    {path: '/testreport',component:require('./components/soiltesting.vue').default},
     {path: '/contact-us',component:require('./components/contactus.vue').default},
     {path: '/soil-report',component:require('./components/soilreport.vue').default},
     {path: '/myrecord',component:require('./components/myrecord.vue').default},
+    {path: '/requestaids',component:require('./components/requestaids.vue').default},
+    {path: '/soiltest',component:require('./components/soiltest.vue').default},
 ]
 
 /**
