c3a0e762efc109778e1acc70d06537224807d898
animeshaks <animeshshukla98279@gmail.com>
2020-07-20 01:05:12 +0530

    Profile Page

---
+++ resources/js/components/profile.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+      <div class="row justify-content-center mt-3">
+		<div class="col-md-8">
+			<div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="row">
+                                        <div class="col-12 col-sm-3">
+                                            <img src="../../../public/image/user.jpg"  height="120" width="120">
+                                        </div> 
+                                        <div class="col-12 col-sm-9">
+                                            <div class="row">
+                                                <div class="col-12 text-center text-sm-left">
+                                                    <h3 class="media-heading mb-0">Name Of User<i class="cursor-pointer bx bxs-star text-warning ml-50 align-middle"></i></h3>
+                                                    <p class="text-muted align-top">Farmer</p>
+                                                </div>
+                                                <div class="col-12 text-center text-sm-left">                               
+                                                    <button class="btn btn-sm d-none d-sm-block float-right btn-primary">
+                                                        <span>Edit</span>
+                                                    </button>   
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
+                        <h5 class="card-title">Basic details</h5>
+                        <ul class="list-unstyled">
+                        <br><br>
+                            <li><i class="fa fa-map-marker mb-1 mr-2" style="color:#666699;"></i>Bihar</li>
+                            <li><i class="fa fa-phone mb-1 mr-2" style="color:#666699;"></i>+9116466446</li>
+                            <li><i class="fa fa-calendar mb-1 mr-2" style="color:#666699;"></i>July 10</li>
+                            <li><i class="fa fa-envelope mb-1 mr-2" style="color:#666699;"></i>abc@gmail.com</li>
+                        </ul>
+                        <div class="row">
+                            <div class="col-6">
+                                <h6><small class="text-muted">Cell Phone</small></h6>
+                                <p>16564664646464</p>
+                            </div>
+                            <div class="col-6">
+                                <h6><small class="text-muted">Full Name</small></h6>
+                                <p>XYZ</p>
+                            </div>
+                            <div class="col-6">
+                                <h6><small class="text-muted">Detail</small></h6>
+                                <p>Kuch Bhi</p>
+                            </div>
+                            <div class="col-6">
+                                <h6><small class="text-muted">Detail</small></h6>
+                                <p>Kuch Bhi</p>
+                            </div>
+                            <div class="col-12">
+                                <h6><small class="text-muted">Bio</small></h6>
+                                <p>dakugsuykdcgfky  ygdcsauyvcaysuc ycsdycvjchvsjcva vcdshcvkjvashcv</p>
+                            </div>
+                        </div>
+                        <button class="btn btn-sm d-none d-sm-block float-right btn-primary">
+                            <span>Edit</span>
+                        </button>  
+                    </div>
+                 </div>
+            </div>
+		</div>
+      </div>    
+  </div>
+</template>
+
+<script>
+export default {
+    mounted()
+    {
+        console.log('Component Mounted')
+    }
+}
+</script>
+
+
+
+
+
+
+
+
+<script>
+export default {
+    mounted()
+    {
+        console.log('Component Mounted')
+    }
+}
+</script>
