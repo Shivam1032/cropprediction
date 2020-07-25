080ce8a90263be1ccdf4412b7dc3069e6f7e903d
animeshaks <animeshshukla98279@gmail.com>
2020-07-26 01:02:49 +0530

    superadmin, admin , farmer dashboard

--- resources/views/farmer/dashboard.blade.php
+++ resources/views/farmer/dashboard.blade.php
@@ -79,12 +79,12 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
-          <a href="{{ route('farmerhome') }}" class="nav-link active">	         
-              <i class="nav-icon fas fa-tachometer-alt"></i>	             
-              <p>	           
-                Dashboard	            
-              </p>	     
-            </a>	      
+              <router-link to="/f_dashboard" class="nav-link active">	         
+                <i class="nav-icon fas fa-tachometer-alt"></i>	             
+                <p>	           
+                  Dashboard	            
+                </p>	     
+            </router-link>
         </li>	     
 
         <li class="nav-item">
